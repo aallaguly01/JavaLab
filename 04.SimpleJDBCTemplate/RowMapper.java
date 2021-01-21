8c69f613a007bcf11449ddb106e38b07664d21c5
aallaguly01 <aallaguly01@gmail.com>
2021-01-21 14:07:34 +0300

    adding old Repository

---
+++ 04.SimpleJDBCTemplate/RowMapper.java
@@ -0,0 +1,9 @@
+//package ru.itis.javalab.repositories.old;
+//
+//import java.sql.ResultSet;
+//import java.sql.SQLException;
+//
+//public interface RowMapper<T> {
+//    T mapRow(ResultSet rom) throws SQLException;
+//
+//}
