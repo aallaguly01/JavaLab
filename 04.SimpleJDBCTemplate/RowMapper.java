a021d0b21def54d1af9fc91db5626a92b31dacf9
aallaguly01 <57533729+aallaguly01@users.noreply.github.com>
2021-01-22 16:05:48 +0300

    Delete RowMapper.java

--- 04.SimpleJDBCTemplate/RowMapper.java
+++
@@ -1,9 +0,0 @@
-//package ru.itis.javalab.repositories.old;
-//
-//import java.sql.ResultSet;
-//import java.sql.SQLException;
-//
-//public interface RowMapper<T> {
-//    T mapRow(ResultSet rom) throws SQLException;
-//
-//}
